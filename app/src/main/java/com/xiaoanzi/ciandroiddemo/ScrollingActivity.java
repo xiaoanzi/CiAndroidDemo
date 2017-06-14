2c925b71df0111d44421e02337fe84b2179863f5
xiaoanzi <xiaoanzi02@hotmail.com>
2017-06-14 11:10:13 +0800

    测试提交啊。。。

--- app/src/main/java/com/xiaoanzi/ciandroiddemo/ScrollingActivity.java
+++ app/src/main/java/com/xiaoanzi/ciandroiddemo/ScrollingActivity.java
@@ -22,7 +22,7 @@ public class ScrollingActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "测试又来啦", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "测试又来啦ahhah", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });

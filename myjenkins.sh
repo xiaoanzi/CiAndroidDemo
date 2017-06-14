11ad24c37c0fd51f09a62ae956b8439e81ec9497
xiaoanzi <xiaoanzi02@hotmail.com>
2017-06-14 10:39:25 +0800

    测试提交构建

--- myjenkins.sh
+++ myjenkins.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-fir publish ./app/build/outputs/apk/app-FIRIM-release.apk -T aefe4883926aaa53d41636ca5ef1097b -c $(cat changelogs/changelog.txt)
+fir publish ./app/build/outputs/apk/app-release-unsigned.apk -T aefe4883926aaa53d41636ca5ef1097b -c $(cat changelogs/changelog.txt)
